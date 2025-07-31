2ab1deae67cf02c511465ae23775151f7b173cc3
duringtheday <developandesignds.com>
2025-07-31 20:12:32 +0700

    GameDuk Certificate App - dependencies updated - 1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gameduk/',
+  base: '/gameduk-microsaas/',
   plugins: [react()],
 })
